d9bc23c53433440a1a9414de0e698f7d87121285
Ayodeji Abimbola <dejibimbola@gmail.com>
2025-05-15 12:39:13 +0100

    Fix import paths for generateGrowthData and add new generateGrowthData utility function
    - Updated import statements in array.mdx and InteractiveRunningTimeGraph.tsx to correct the path for generateGrowthData.
    - Added a new utility function, generateGrowthData, in a new file to calculate growth data based on complexity types.

--- app/content/components/InteractiveRunningTimeGraph.tsx
+++ app/content/components/InteractiveRunningTimeGraph.tsx
@@ -7,7 +7,7 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { generateGrowthData, ComplexityType } from "@/lib/generatateGrowthData";
+import { generateGrowthData, ComplexityType } from "@/lib/generateGrowthData";
 
 import { RunningTimeGraph } from "./RunningTimeGraph";
 
